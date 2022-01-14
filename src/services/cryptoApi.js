35be438e6ca7c55a156836cfebe1c1778c60e4e7
VanHungNguyen <vanhungnguyenbku@gmail.com>
2022-01-14 17:51:04 +0700

    .

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,1 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const cryptoApiHeaders = {
+	'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+	'x-rapidapi-key': '0241cc4366mshed8af4e5d6bf334p16c45fjsnecbc3a68e925',
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: { cryptoApiHeaders } })
+
+export const cryptoApi = createApi({
+	reducerPath: 'cryptoApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl,
+	}),
+	endpoints: (builder) => ({
+		getCryptos: builder.query({
+			query: () => createRequest('/exchanges'),
+		}),
+	}),
+})
